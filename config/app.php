9f22f1fab29dca5a4f8156c5424346387b867a95
Per Kristian <kristiansvanberg@gmail.com>
2019-02-17 16:43:32 +0100

    file creation for a school project

---
+++ config/app.php
@@ -0,0 +1,16 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', '_USERNAME_');
+define('DB_PASSWORD', '_PASSWORD_');
+define('DB_NAME', '_NAME_OF_THE_DB');
+
+// Site title
+define('SITE_TITLE', 'Monkiefy');
+
+// vet ej om denna behövs:
+// App root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL root
+define('URLROOT', '__HERE_YOU_PUT_YOUR_PATH_');
